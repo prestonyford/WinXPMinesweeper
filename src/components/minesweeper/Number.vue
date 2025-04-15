5ca3489ea80475ab8fd6d378568ad7317195ec61
Preston Ford <preston.y.ford@gmail.com>
2025-04-14 19:35:45 -0600

    Code quality

--- src/components/minesweeper/Number.vue
+++ src/components/minesweeper/Number.vue
@@ -1,15 +1,5 @@
 <template>
-	<!-- Yeah. I know. -->
-	<img v-if="number === 0" src="../../assets/Number0.png">
-	<img v-else-if="number === 1" src="../../assets/Number1.png">
-	<img v-else-if="number === 2" src="../../assets/Number2.png">
-	<img v-else-if="number === 3" src="../../assets/Number3.png">
-	<img v-else-if="number === 4" src="../../assets/Number4.png">
-	<img v-else-if="number === 5" src="../../assets/Number5.png">
-	<img v-else-if="number === 6" src="../../assets/Number6.png">
-	<img v-else-if="number === 7" src="../../assets/Number7.png">
-	<img v-else-if="number === 8" src="../../assets/Number8.png">
-	<img v-else src="../../assets/Number9.png">
+	<img :src="`/src/assets/Number${number}.png`">
 </template>
 
 <script lang="ts">
