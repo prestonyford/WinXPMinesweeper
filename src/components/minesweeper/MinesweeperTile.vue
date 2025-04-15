df364a0845f4c7c020255b0ffdd59d93717a01cc
Preston Ford <preston.y.ford@gmail.com>
2025-04-14 19:32:04 -0600

    Pixel art

--- src/components/minesweeper/MinesweeperTile.vue
+++ src/components/minesweeper/MinesweeperTile.vue
@@ -2,13 +2,20 @@
 	<button 
 		class="tile flex items-center justify-center"
 		:class="{
-			'zero': tile.type === TileType.NUMBER && tile.value === 0,
+			'unopened': tile.type === TileType.UNOPENED,
+			'number': tile.type === TileType.NUMBER,
 			'flag': tile.type === TileType.FLAGGED
 		}"
 	>
-		<div v-if="tile.type === TileType.BOMB">B</div>
-		<div v-if="tile.type === TileType.FLAGGED">F</div>
-		<div v-else-if="tile.type === TileType.NUMBER">{{ tile.value || "" }}</div>
+		<div v-if="tile.type === TileType.BOMB">
+			<img src="/src/assets/Bomb.png" />
+		</div>
+		<div v-if="tile.type === TileType.FLAGGED">
+			<img src="/src/assets/Flag.png" />
+		</div>
+		<div v-else-if="tile.type === TileType.NUMBER" class="pt-[1px] pl-[1px]" >
+			<img v-if="tile.value !== 0" :src="`/src/assets/Tile${tile.value}.png`" />
+		</div>
 	</button>
 </template>
 
@@ -28,17 +35,21 @@ defineProps<{
 	border-top: 2px white solid;
 	border-right: 2px #808080 solid;
 	border-bottom: 2px #808080 solid;
+	image-rendering: pixelated;
 }
-.tile:active:not(.flag), .zero {
+.tile:active:not(.flag), .number {
 	background-color: #adadad;
 	border-left: 2px #808080 solid;
 	border-top: 2px #808080 solid;
 	border-right: 2px #adadad solid;
 	border-bottom: 2px #adadad solid;
 }
-.tile:active:not(.flag) > * {
+.unopened:active > * {
 	position: relative;
 	top: 1px;
 	left: 1px;
+}
+img {
+	pointer-events: none;
 }
 </style>
